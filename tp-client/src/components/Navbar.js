929782b9d64dd7e625c0466c86cb5d830c964f9c
Howakyl96 <howakyl@gmail.com>
2020-11-23 14:02:02 -0800

    navbar can route to index component

--- tp-client/src/components/Navbar.js
+++ tp-client/src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar(props) {
                         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li> */}
                     <li className="nav-item">
-                        <a className="nav-link" href="/">View TP</a>
+                        <NavLink className="nav-link" to="/index">View TP</NavLink>
                     </li>
                 </ul>
                 <form className="form-inline my-2 my-md-0">
