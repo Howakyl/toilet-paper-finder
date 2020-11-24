cfec747dc04d37fe99769916ead65d65e0785333
Howakyl96 <howakyl@gmail.com>
2020-11-23 20:12:26 -0800

    started home page

--- tp-client/src/pages/Home.js
+++ tp-client/src/pages/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Home  = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Welcome!</h1>
+            <h3>View TP Availibility By:</h3>
         </div>
     );
 };
